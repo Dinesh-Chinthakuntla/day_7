4acb42e59f30c0b584a0973ffffa190a1f54eab6
Dinesh Chinthakuntla <dineshchinthakuntla@gmail.com>
2025-07-05 16:31:27 +0530

    logical operators

---
+++ logical_operators.py
@@ -0,0 +1,35 @@
+#and operator
+
+book=True
+pen=False
+print(book and pen)
+
+book=True
+pen=True
+print(book and pen)
+
+book=False
+pen=False
+print(book and pen)
+
+book=False
+pen=True
+print(book and pen)
+
+book=True
+pen=False
+print(book or pen)
+
+book=True
+pen=True
+print(book or pen)
+
+book=False
+pen=False
+print(book or pen)
+
+book=False
+pen=True
+print(book or pen)
+
+
---
+++ or_operator.py
@@ -0,0 +1,16 @@
+book=True
+pen=False
+print(book or pen)
+
+book=True
+pen=True
+print(book or pen)
+
+book=False
+pen=False
+print(book or pen)
+
+book=False
+pen=True
+print(book or pen)
+
